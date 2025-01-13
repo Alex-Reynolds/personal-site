07fd048569b95b5ff0f09333b40cd033adea57d5
Alex Reynolds <awr@iastate.edu>
2025-01-13 12:12:10 -0700

    trying to switch to docs folder to see if that fixes pages

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 // Serve static files from the 'public' folder and node_modules
-app.use(express.static('public'));
+app.use(express.static('docs'));
 app.use('/node_modules', express.static('node_modules'));
 
 app.listen(port, () => {
